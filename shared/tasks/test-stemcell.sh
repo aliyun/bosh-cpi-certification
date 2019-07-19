92e19acbb6ed53ab2cbe0ed46ce0917476349eaa
He Guimin <guimin.hgm@alibaba-inc.com>
2019-07-19 16:04:35 +0800

    add bats scripts for alibaba cloud CPI

--- shared/tasks/test-stemcell.sh
+++ shared/tasks/test-stemcell.sh
@@ -18,6 +18,13 @@ export BOSH_stemcell_version=\"$(realpath stemcell/version | xargs -n 1 cat)\"
 pushd bosh-linux-stemcell-builder
   export PATH=/usr/local/go/bin:$PATH
   export GOPATH=$(pwd)
+  echo -e "Rebasing with the official latest code until the issue bosh-linux-stemcell-builder/issues/93 and /bosh-linux-stemcell-builder/pull/89 are fixed"
+  git branch
+  git remote add bosh https://github.com/cloudfoundry/bosh-linux-stemcell-builder.git
+  git remote -v
+  git pull --rebase bosh master
+
+  echo -e "Rebase finished."
 
   pushd src/github.com/cloudfoundry/stemcell-acceptance-tests
     ./bin/test-smoke $package
